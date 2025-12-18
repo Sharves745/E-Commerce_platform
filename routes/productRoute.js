92ea11e4dc8831caeb93ec89c2e2fb332af7f3f8
Sharves745 <sharves2006cr7@gmail.com>
2025-12-18 19:51:39 +0530

    Update productRoute.js
    Update productRoute.js

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { getProduct, postProduct, deleteProduct, updateProduct } = require("../controller/productController");
+const {
+  getProduct,
+  postProduct,
+  deleteProduct,
+  updateProduct
+} = require("../controller/productController");
 
 const router = express.Router();
 
-router.get("/getproduct", getProduct);
-router.post("/postProduct",postProduct)
-router.delete("/deleteProduct/:id",deleteProduct)
-router.put("/updateProduct/:id",updateProduct)
+router.get("/products", getProduct);
+router.post("/products", postProduct);
+router.delete("/products/:id", deleteProduct);
+router.put("/products/:id", updateProduct);
+
 module.exports = router;
